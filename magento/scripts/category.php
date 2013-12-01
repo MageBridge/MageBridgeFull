4d9c113ec7fc05ce187f9a23d3cb5bdd5ad26408
Jisse Reitsma <info@yireo.com>
2013-12-01 14:40:58 +0100

    Pending changes

--- magento/scripts/category.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'app/Mage.php';
-Mage::app();
-
-$root = Mage::getModel('catalog/category')->load(3);
-print_r($root->debug());
-echo "\n";
--- magento/scripts/customer.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../app/Mage.php';
-Mage::app(1, 'website');
-
-$customerId = 55;
-$customer = Mage::getModel('customer/customer')->load($customerId);
-Mage::dispatchEvent('customer_save_after', array('customer' => $customer));
-echo "\n";
