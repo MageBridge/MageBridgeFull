55fa002e1b2aaa20bf06fddaedc0621abeddb131
Jisse Reitsma <info@yireo.com>
2014-02-14 16:00:28 +0100

    Pending changes

---
+++ build/includes/Yireo_MageBridge_installer.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+'base_dir'               => '/apache/git/software/magebridge/MageBridgeFull/magento/var/installer',
+'archive_files'          => 'Yireo_MageBridge.zip',
+'extension_name'         => 'Yireo_MageBridge',
+'extension_version'      => null,
+'path_output'            => '/apache/git/software/magebridge/MageBridgeFull/magento/var/installer',
+'stability'              => 'stable',
+'license'                => 'Open Source License',
+'channel'                => 'community',
+'summary'                => 'No summary',
+'description'            => 'No description',
+'notes'                  => 'No notes',
+'author_name'            => 'Yireo',
+'author_user'            => 'yireo',
+'author_email'           => 'info@yireo.com',
+'php_min'                => '5.2.0',
+'php_max'                => '6.0.0',
+'auto_detect_version'    => true,
+'skip_version_compare'   => false,
+);
