ae7c2411988988df4637ea48938f98a8e577d1aa
Jisse Reitsma <info@yireo.com>
2014-05-05 09:39:43 +0200

    Pending changes

---
+++ joomla2/administrator/components/com_magebridge_importer/views/fieldsets/tmpl/thead.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ * Joomla! component MageBridge
+ *
+ * @author Yireo (info@yireo.com)
+ * @package MageBridge
+ * @copyright Copyright 2014
+ * @license GNU Public License
+ * @link http://www.yireo.com
+ */
+
+defined('_JEXEC') or die('Restricted access');
+?>
+<th width="160" class="title">
+    <?php echo JText::_('COM_MAGEBRIDGE_IMPORTER_MODEL_FIELD_FIELD_LABEL'); ?>
+</th>
+<th width="160" class="title">
+    <?php echo JText::_('COM_MAGEBRIDGE_IMPORTER_MODEL_FIELD_FIELD_NAME'); ?>
+</th>
+<th width="160" class="title">
+    <?php echo JText::_('COM_MAGEBRIDGE_IMPORTER_MODEL_FIELD_FIELD_PROFILE_ID'); ?>
+</th>
