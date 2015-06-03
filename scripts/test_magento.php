ca9fe73218cea642879fd4e10c9dd4f968b950a5
Jisse Reitsma <info@yireo.com>
2015-06-03 09:34:01 +0200

    Committing changes for version 1.9.5309

--- scripts/test_magento.php
+++ scripts/test_magento.php
@@ -1,3 +1,3 @@
 <?php 
-$string = 'aHR0cDovL29mZnlhdHJlZXJlY29yZHMuZGUvb25saW5lc2hvcC9oaXR6ZWZyZWktNjA_U0lEPQ,,';
+$string = 'aHR0cHM6Ly9pcm9uc2hpcnRvZmdvbGYuY29tL3N0b3JlL2NhdGFsb2cvcHJvZHVjdC92aWV3L2Zvcm1fa2V5L002QzMwYXhWd3JjSDh0Zk0vaWQvMi8v';
 echo base64_decode(strtr($string, '-_,', '+/='))."\n";
